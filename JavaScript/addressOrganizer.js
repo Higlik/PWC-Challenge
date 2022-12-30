3c4afafbde9038362c573bb43639feb1450649b1
Higlik <victor.silva.franca18@gmail.com>
2022-12-30 19:55:19 -0300

    finish challenge

--- JavaScript/addressOrganizer.js
+++ JavaScript/addressOrganizer.js
@@ -3,43 +3,41 @@ const btn = document.querySelector("#send")
     btn.addEventListener("click", function (e) {
         e.preventDefault()
         
-        //captar o endereço
+        //captura o endereço digitado
         const addressColector = document.querySelector("#addressColector");
+        
         //atribuir o endereço em value
         var value = addressColector.value;
-
-        console.log(value);
+        
         //executa a troca da vírgula
         var replacement = /,/g;
+        
         //Armazena o endereço
         var container = [];
         
+        //Remove a(s) vírgula(s) por espaço em branco
         value = value.replace(replacement,"");
 
-        console.log(value);
-        //regex para filtrar o endereço
+        //regex para filtrar o se não há erro
         exp = /^(\s*|[\d\s]*)$/i;
 
-        
-        //Realiza a função do regex
+        //Realiza a função do regex e atribui em result
         result = exp.exec(value);
-        //Apaga o primeiro indice do conteiner
         
 
-
-        //Valida se a pesquisa atende endereço simples
+        //validação para erro de endereço
         if(exp.test(value) == true) {
             container.push("Endereço digitado errado");
-            console.log("Aqui 1");
 
+        //validação para ruas compostas com números
         }else if(exp.test(value) == false){
-            exp = /^([a-zçíãáõé]+\s\d+)+? ([a-z][\w+\s]+)$/i;
+            exp = /^([a-zçíãáõé]+\s\d+\w?)+? ([a-z][\w+\s]+)$/i;
             result = exp.exec(value);
             if(exp.test(value) == true){
                 container.push(result);
                 result.shift();
-                console.log("Aqui 1");
             }else{
+                //validação para endereços que iniciam com números
                 if(exp.test(value) == false){
                     exp = /^(\d+[a-zçíãáõé]?\s+)([a-zçíãáõé\s]+)$/i
                     result = exp.exec(value);
@@ -47,8 +45,8 @@ const btn = document.querySelector("#send")
                         container.push(result[2]);
                         container.push(result[1]);
                         result.shift();
-                        console.log("Aqui 2");
                     }else{
+                        //validação para endereços simples e com números compostos de letras
                         exp = /^([a-zçíãáõé\s?]+) ([\d\sa-z]+)$/i;
                         result = exp.exec(value);
                         if(exp.test(value) == true){
@@ -61,12 +59,11 @@ const btn = document.querySelector("#send")
                 }
             }
         }else{
+            //Caso ocorra um erro não esperado
             container.push("Endereço digitado errado");
         }
 
         //Printa na text area o resultado da regex
-            var address =  "Endereço: "+container;
+            var address =  "Endereço: "+ container;
             document.getElementById("showAdrress").innerHTML = address; 
-
-
     });
